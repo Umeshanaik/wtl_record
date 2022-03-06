b42f277184074f53a46ae7ddcb0f7db2f39fe3dd
Umesh A Nayik <umeshnaik2k@gmail.com>
2022-03-06 07:22:18 +0530

    prime.php
    to check whether it is prime

--- prime.php
+++ prime.php
@@ -1,21 +1,26 @@
-<?php  
-$count = 0;  
-$num = 2;  
-while ($count < 15 )  
-{  
-$div_count=0;  
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$num = 13;  
+$count=0;  
 for ( $i=1; $i<=$num; $i++)  
 {  
 if (($num%$i)==0)  
 {  
-$div_count++;  
+$count++;  
 }  
 }  
-if ($div_count<3)  
+if ($count<3)  
 {  
-echo $num." , ";  
-$count=$count+1;  
-}  
-$num=$num+1;  
-}  
-?>  
+echo "$num is a prime number.";  
+}
+else
+{
+echo "$num is not a prime number."; 
+}
+?>
+ 
+</body>
+</html>
